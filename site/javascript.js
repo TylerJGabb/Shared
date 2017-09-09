e5aa7600349f885837ea10e0861dfe474efc1d81
Tyler <tylerjgabb@gmail.com>
2017-09-08 18:22:57 -0700

    add javascript

--- site/javascript.js
+++ site/javascript.js
@@ -1,2 +1,13 @@
-
 console.log("some quote blap blip foo bar baz");
+function btn1_clicked(){
+    d3.select("body")
+        .style("background","red")
+        .append("svg")
+        .append("circle")
+            .attr("cx",20)
+            .attr("cy",20)
+            .attr("r",50)
+            .style("fill","darkblue");
+}
+
+d3.select("#btn1").attr("onclick","btn1_clicked()");
